2185603a9ae6927fe88e28e442e60e6cd68aef00
altamarmx <gbv@ier.unam.mx>
2021-06-02 17:00:43 -0500

    groupby and classes

---
+++ notebooks/.ipynb_checkpoints/clase-checkpoint.py
@@ -0,0 +1,7 @@
+class Tools:
+    def __init__(self, nombre="memo",apellido="barrios"):
+        self.nombre = nombre
+        self.apellido = apellido
+    def saluda(self):
+        print("Hola mundo")
+#         print(self.nombre)
